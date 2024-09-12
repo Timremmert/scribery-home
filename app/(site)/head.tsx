03c8286006069555df1de8e49ce1e652ce7093a2
Tim Remmert <remmert567@gmail.com>
2024-09-12 18:11:26 +0200

    Update fav

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title> Solid | Next.js Template for Startup and SaaS</title>
+      <title>Scribery - AI LinkedIn Post Scheduler</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="/images/favicon.png" />
     </>
   );
 }
