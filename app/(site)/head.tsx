1f69470661408c25576e1243130df927ae6f5de3
Tim Remmert <remmert567@gmail.com>
2024-09-12 18:19:43 +0200

    fav2

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Scribery - AI LinkedIn Post Scheduler</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
-      <link rel="icon" href="../favicon.png" />
+      <link rel="icon" href="/images/favicon.png" />
     </>
   );
 }
