6dd41381e830df482f05c909691563073a1f297e
Tim Remmert <remmert567@gmail.com>
2024-09-13 11:09:24 +0200

    Update Hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,41 +16,19 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 SCRIBERY - GPT-4 BASED POST SCHEDULER
+                🚀 Scribery - GPT-4o Based Post Scheduler
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Boost your {"   "} presence. 
+                Boost your {"   "}  
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  LinkedIn
+                  LinkedIn profile.
                 </span>
               </h1>
               <p>
                   Quickly create posts from various categories and customize them to your business's needs.
               </p>
-
-              <div className="mt-10">
-                <form onSubmit={handleSubmit}>
-                  <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="text"
-                      placeholder="Enter your email address"
-                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
-                    <button
-                      aria-label="get started button"
-                      className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
-                    >
-                      Get Started
-                    </button>
-                  </div>
-                </form>
-
-                <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
-                </p>
-              </div>
+              
+              
             </div>
 
             <div className="animate_right hidden md:w-1/2 lg:block">
